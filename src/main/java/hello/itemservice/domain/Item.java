75cf18a70d12ff20de138230bde0452fda487dc2
isu-nice <isu010614@naver.com>
2024-02-04 14:16:17 +0900

    feat: JPA 관련 애노테이션 추가

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,15 +2,22 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
 
+    // JPA를 사용하려면 기본 생성자 꼭 필요함
     public Item() {
     }
 
