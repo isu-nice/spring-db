c6992a68f0e5745aad049638c8c597201baa84d2
isu-nice <isu010614@naver.com>
2024-02-07 23:09:08 +0900

    feat: Spring Data JPA repository 구현

---
+++ src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,24 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+
+    // findByItemName -> 정확히 일치하는 것만 찾음
+    // findByItemNameLike -> 해당 문자열이 포함된 모든 항목을 찾음
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEqual(Integer price);
+
+    // 쿼리 메서드 (아래 메서드와 같은 기능 수행)
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    // 쿼리 직접 실행
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+}
